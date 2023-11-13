5dad454b781de4319d318f920b93cb6410e97103
Sarthak <77429824+sarthakdevil@users.noreply.github.com>
2023-11-13 13:39:44 +0530

    Add files via upload

---
+++ codsoft/calculator/logic.js
@@ -0,0 +1,14 @@
+ let result;  
+if (operator == '+') { 
+    result = number1 + number2;  
+}  
+else if (operator == '-') { 
+    result = number1 - number2;  
+}  
+else if (operator == '*') { 
+    result = number1 * number2;  
+}  
+else {  
+    result = number1 / number2; 
+}  
+window.alert(" Result is" + result);  
